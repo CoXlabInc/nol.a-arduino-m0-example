947e65345244ddabd7b6922b545ffdcd51272834
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-05-24 14:38:56 +0900

    Added testing gettimeofday().

--- Basic/main.cpp
+++ Basic/main.cpp
@@ -4,7 +4,9 @@ Timer tPrint;
 
 static void printTask(void *) {
   System.ledToggle();
-  printf("[%lu usec] Hi!\n", micros());
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  printf("[%lu.%06lu] Hi!\n", (uint32_t) t.tv_sec, t.tv_usec);
 }
 
 static void keyboard(SerialPort&) {
@@ -16,9 +18,17 @@ void setup() {
   System.ledOn();
   Serial.begin(115200);
   printf("\n*** Basic functions test for Arduino-M0 ***\n");
+  struct tm t;
+  t.tm_year = 2018 - 1900;
+  t.tm_mon = 5 - 1;
+  t.tm_mday = 23;
+  t.tm_hour = 0;
+  t.tm_min = 0;
+  t.tm_sec = 0;
+  System.setDateTime(t);
 
   tPrint.onFired(printTask, NULL);
-  tPrint.startPeriodic(1000);
+  tPrint.startPeriodic(100);
 
   Serial.listen();
   Serial.onReceive(keyboard);
